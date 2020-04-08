edcb5e62990bb6d947bfed89a2b015fcd39b0b30
katenalinthip2 <32117599+katenalinthip2@users.noreply.github.com>
2020-04-08 19:53:45 +0700

    Routes login and App
    Routes ไปหน้า login กับ App

---
+++ src/Routes.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Login from "./Login";
+import App from "./App";
+
+const Routes = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/App" exact component ={App} />
+                <Route path="/Login" exact component ={Login} />
+                
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+export default Routes;
