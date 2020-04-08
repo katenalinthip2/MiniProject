0ba63c70b626cc9da56dfe8ed20d2a0b98fb6b57
katenalinthip2 <32117599+katenalinthip2@users.noreply.github.com>
2020-04-08 19:38:21 +0700

    render App --> Routes
    แก้ไขการ render เป็น Routes

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import Routes from './Routes';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Routes />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+serviceWorker.unregister();
