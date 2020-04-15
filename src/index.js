91b81ede86e73440f7c524173e2e02887cc83fd2
katenalinthip2 <32117599+katenalinthip2@users.noreply.github.com>
2020-04-16 02:35:31 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Routes from './Routes';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import firebase from 'firebase';
+import 'firebase/firestore'
+import config from './firebase/config'
+
+if (firebase.apps.length === 0)
+   firebase.initializeApp(config);
+export const firestore = firebase.firestore()
 
 ReactDOM.render(
   <React.StrictMode>
