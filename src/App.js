4a48b7f2253e9b50ce2dd4911f533e8428802289
katenalinthip2 <32117599+katenalinthip2@users.noreply.github.com>
2020-04-16 01:50:49 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React , { useState, useEffect }from 'react';
+import './App.css';
+import Nav from './Nav';
+import {firestore} from './index'
+
+const App = () => {
+
+  const [collects, setCollects] = useState([
+    { id: 1, name: "Glass" },
+    { id: 2, name: "Bottle" }
+  ])
+
+  useEffect( ()=> {
+    retriveData()
+  } ,[])
+
+  const retriveData = () => {
+    firestore.collection("collects").onSnapshot((snapshot) => {
+      console.log(snapshot.docs)
+      let myCollect = snapshot.docs.map(d => {
+        const { id, name } = d.data()
+        console.log(id, name)
+        return { id, name }
+      })
+      setCollects(myCollect)
+    })
+  }
+
+  const renderCollect = () => {
+    if (collects && collects.length)
+      return collects.map((collect, index) => {
+        return (
+          <li key={index}>
+            {collect.id} : {collect.name}
+          </li>
+          
+        )
+      })
+    else
+      return (<li> No Task </li>)
+  }
+  
   return (
-    <div className="App">
-        <h1> Hello </h1>
+  <div  >
+      <Nav />
+     <h1 > My Collection </h1>
+     <ul >{renderCollect()}</ul>
     </div>
   );
 }
