df888df3defaac9f45d91673045a9deeb9f1963c
katenalinthip2 <32117599+katenalinthip2@users.noreply.github.com>
2020-04-29 00:29:26 +0700

    Add files via upload

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,6 @@
 import 'firebase/storage';
+import 'firebase/auth'
 import firebase from 'firebase/app'
-
 const firebaseConfig = {
   apiKey: "AIzaSyDcBpE1SygJcrXDmngTXW2BT8vCtPUTB0s",
   authDomain: "loginminipro.firebaseapp.com",
@@ -11,9 +11,7 @@ const firebaseConfig = {
   appId: "1:951579270707:web:3dea7e69aeb1e2129d8d46",
   measurementId: "G-62CS4P3CG4"
 };
-
 firebase.initializeApp(firebaseConfig);
 const storage = firebase.storage();
-
-export {firebase, storage as default}
-
+const auth = firebase.auth();
+export {firebase, storage, auth as default}
